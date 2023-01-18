f951f86d58e9aec2c4483e1bdb40cb115a4be2f2
mabono <maciej.bonowicz@kaseya.com>
2023-01-18 08:48:42 +0100

    Update on first task
    Added rewrite for the ingress, to access default routes in the apis

--- task1/api1/api1.py
+++ task1/api1/api1.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api1')
+@app.route('/')
 def hello():
     return "api1"
 
--- task1/api2/api2.py
+++ task1/api2/api2.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api2')
+@app.route('/')
 def hello():
     return "api2"
 
